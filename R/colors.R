c3aff7bd6bf27f83f40881e7df6b80e062af852d
ejc58 <edwardjcarr@hotmail.com>
2021-05-22 09:33:30 +0100

    Add scale_colour_taylor

--- R/colors.R
+++ R/colors.R
@@ -50,6 +50,25 @@ scale_color_taylor <- function(palette = "taylor1989",
   }
 }
 
+#' Colour scale for Taylor Swift colors
+#'
+#' @param palette Character name of palette in swift_palettes
+#' @param discrete Boolean if color aesthetic is discrete
+#' @param reverse Boolean indicating whether palette should be reversed
+#' @param ... Additional arguments used to discrete_scale() or scale_fill_gradientn()
+#'   to automatically interpolate between colours.
+#' @export
+scale_colour_taylor <- function(palette = "taylor1989",
+                                discrete = TRUE, reverse=FALSE,...){
+  pal <- swift_pal(palette = palette, reverse = reverse)
+
+  if(discrete){
+    ggplot2::discrete_scale("colour", paste0("swift_", palette), palette = pal, ...)
+  }else{
+    ggplot2::scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
 #' Fill scale for Taylor Swift colors
 #'
 #' @param palette Character name of palette in swift_palettes
